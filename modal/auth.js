439f77823502b6f5a917d783e219066554ced22d
unknown <shivamswami8899@gmail.com>
2023-11-23 22:43:45 +0530

    changes done

--- modal/auth.js
+++ modal/auth.js
@@ -6,7 +6,7 @@ const authSchema = mongoose.Schema({
   password: { type: String, required: true },
   country: { type: String, required: true },
 });
-
+// mera desh badal rha hai
 const AuthModal = mongoose.model("user", authSchema);
 
 module.exports = { AuthModal };
